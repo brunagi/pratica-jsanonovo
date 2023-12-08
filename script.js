e6307818196abe701ea46b06e5619433e4f762e1
brunagi <brunnabrs97@gmail.com>
2023-12-08 15:22:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Ano novo está chegando!!");
+
+let cantora = "Mariah Carey";
+
+let idade = 54;
+
+let ano = 2023;
+
+
+const anoNascimento = ano - idade;
+console.log(anoNascimento);
+
+let quartaFeira = true;
+
+console.log(typeof quartaFeira);
+
+let idade2 = 15;
+
+if ( idade2 >= 18) {
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.!")
+} else{
+    console.log("Sinto muito, mas assistirá na TVGlobo")
+}
+let aniver = "Agosto";
+
+if (aniver == "Agosto" || aniver == "Dezembro" || aniver == "Junho") {
+console.log(`Uma ou mais opções estão corretas. O mês escolhido foi ${aniver}`)
+} else {
+    console.log(`Algo de errado não está certo, o mês digitado foi ${aniver}`)
+}
